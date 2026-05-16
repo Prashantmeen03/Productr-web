72a647f267acf505ebb86fa10eb4328a546edd05
prashantdagal03 <prashantdagal03@gmail.com>
2026-05-16 11:10:23 +0530

    Fix Express 5 PathError in server.js

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 // API Routes (already defined above)
 
 // Catch-all to serve frontend index.html
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
